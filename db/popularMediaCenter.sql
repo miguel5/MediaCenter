77c08861f982ad501e9ed64e993ae635e8820948
miguel5 <miguelarrepia@gmail.com>
2019-12-16 02:40:54 +0000

    idAmigo também é chave primária / alterações nas queries

--- db/popularMediaCenter.sql
+++ db/popularMediaCenter.sql
@@ -1,19 +1,19 @@
 USE mediaCenter;
 
-INSERT INTO Utilizador VALUES (1,'Zé Admin','zeadmin@gmail.com','pass1',1);
-INSERT INTO Utilizador VALUES (2,'Cristiano Ronaldo','cristianoronaldo@gmail.com','pass2',0);
-INSERT INTO Utilizador VALUES (3,'Bernardo Silva','bernardo silva@gmail.com','pass3',0);
-INSERT INTO Utilizador VALUES (4,'Bruno Lage','brunolage@gmail.com','pass4',0);
-INSERT INTO Utilizador VALUES (5,'Bruno Fernandes','brunofernandes@gmail.com','pass5',0);
+INSERT INTO Utilizador (nome, email, password, admin) VALUES ('Zé Admin','zeadmin@gmail.com','pass1',1);
+INSERT INTO Utilizador (nome, email, password, admin) VALUES ('Cristiano Ronaldo','cristianoronaldo@gmail.com','pass2',0);
+INSERT INTO Utilizador (nome, email, password, admin) VALUES ('Bernardo Silva','bernardo silva@gmail.com','pass3',0);
+INSERT INTO Utilizador (nome, email, password, admin) VALUES ('Bruno Lage','brunolage@gmail.com','pass4',0);
+INSERT INTO Utilizador (nome, email, password, admin) VALUES ('Bruno Fernandes','brunofernandes@gmail.com','pass5',0);
 
-INSERT INTO Conteudo VALUES (1,'A garagem da Vizinha','/HOME/user/conteudo/','Quim Barreiros','Pimba',0);	
-INSERT INTO Conteudo VALUES (2,'Paranoid Android','/HOME/user/conteudo/','Radiohead','Rock Alternativo',0);	
-INSERT INTO Conteudo VALUES (3,'Jeremy','/HOME/user/conteudo/','Pearl Jam','Rock Alternativo',0);	
-INSERT INTO Conteudo VALUES (4,'Stairway to Heaven','/HOME/user/conteudo/','Led Zeppelin','Rock',0);	
-INSERT INTO Conteudo VALUES (5,'Videoclip da garagem da Vizinha','/HOME/user/conteudo/','Quim Barreiros','Pimba',1);	
-INSERT INTO Conteudo VALUES (6,'Videoclip Paranoid Android','/HOME/user/conteudo/','Radiohead','Rock Alternativo',1);	
-INSERT INTO Conteudo VALUES (7,'Videoclip Jeremy','/HOME/user/conteudo/','Pearl Jam','Rock Alternativo',1);	
-INSERT INTO Conteudo VALUES (8,'Videoclip Stairway to Heaven','/HOME/user/conteudo/','Led Zeppelin','Rock',1);
+INSERT INTO Conteudo (titulo, localizacao, artista, genero, tipo) VALUES ('A garagem da Vizinha','/HOME/user/conteudo/','Quim Barreiros','Pimba',0);	
+INSERT INTO Conteudo (titulo, localizacao, artista, genero, tipo) VALUES ('Paranoid Android','/HOME/user/conteudo/','Radiohead','Rock Alternativo',0);	
+INSERT INTO Conteudo (titulo, localizacao, artista, genero, tipo) VALUES ('Jeremy','/HOME/user/conteudo/','Pearl Jam','Rock Alternativo',0);	
+INSERT INTO Conteudo (titulo, localizacao, artista, genero, tipo) VALUES ('Stairway to Heaven','/HOME/user/conteudo/','Led Zeppelin','Rock',0);	
+INSERT INTO Conteudo (titulo, localizacao, artista, genero, tipo) VALUES ('Videoclip da garagem da Vizinha','/HOME/user/conteudo/','Quim Barreiros','Pimba',1);	
+INSERT INTO Conteudo (titulo, localizacao, artista, genero, tipo) VALUES ('Videoclip Paranoid Android','/HOME/user/conteudo/','Radiohead','Rock Alternativo',1);	
+INSERT INTO Conteudo (titulo, localizacao, artista, genero, tipo) VALUES ('Videoclip Jeremy','/HOME/user/conteudo/','Pearl Jam','Rock Alternativo',1);	
+INSERT INTO Conteudo (titulo, localizacao, artista, genero, tipo) VALUES ('Videoclip Stairway to Heaven','/HOME/user/conteudo/','Led Zeppelin','Rock',1);
 
 INSERT INTO Utilizador_has_Conteudo VALUES (1,1);
 INSERT INTO Utilizador_has_Conteudo VALUES (2,2);
@@ -30,8 +30,8 @@ INSERT INTO Utilizador_has_Conteudo VALUES (4,8);
 INSERT INTO Utilizador_has_Conteudo VALUES (5,1);
 INSERT INTO Utilizador_has_Conteudo VALUES (5,5);
 
-INSERT INTO Playlist VALUES (1,'Playlist do Cristi',2);
-INSERT INTO Playlist VALUES (2,'Playlist Led Zeppelin',2);
+INSERT INTO Playlist (nome, Utilizador_idUtilizador) VALUES ('Playlist do Cristi',2);
+INSERT INTO Playlist (nome, Utilizador_idUtilizador) VALUES ('Playlist Led Zeppelin',2);
 
 INSERT INTO Playlist_has_Conteudo VALUES (1,2);
 INSERT INTO Playlist_has_Conteudo VALUES (1,4);
